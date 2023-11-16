d7abca4913abfdab2b17c42ba9b2135b37b66f61
jk <jakubykriz@gmail.com>
2023-11-16 11:35:20 +0100

    add save sessions to file

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
 const defaultSessionDuration time.Duration = time.Duration(1 * time.Minute)
 
 type session struct {
-	start    time.Time
-	duration int
-	category string
+	Start    time.Time `json:"start"`
+	Duration int       `json:"duration"`
+	Category string    `json:"category"`
 }
 
 func printElapsed(d time.Duration) {
@@ -29,10 +32,39 @@ func runSession(duration time.Duration, category string) session {
 		time.Sleep(100 * time.Millisecond)
 		elapsed = time.Since(startTime)
 	}
-	return session{start: startTime, duration: int(duration), category: category}
+	return session{Start: startTime, Duration: int(duration.Minutes()), Category: category}
+}
+
+func loadSessions(filepath string) (string, error) {
+	content, err := ioutil.ReadFile(filepath)
+	return string(content), err
+}
+
+func saveSessions(filepath string, content string) error {
+	f, err := os.Open(filepath)
+	defer f.Close()
+	if err == nil {
+		return err
+	}
+	f.WriteString(content)
+	return nil
 }
 
 func main() {
+	s, err := loadSessions("sessions.json")
+	if err != nil {
+		fmt.Println("error loading sessions")
+	}
+	fmt.Println(s)
+	allSessions := []session{}
+	json.Unmarshal([]byte(s), &allSessions)
+	fmt.Println(allSessions)
+
 	session := runSession(5*time.Second, "unknown")
-	fmt.Print(session)
+	allSessions = append(allSessions, session)
+	fmt.Println(allSessions)
+
+	sessionsJson, _ := json.Marshal(allSessions)
+	saveSessions("sessions.json", string(sessionsJson))
+
 }
